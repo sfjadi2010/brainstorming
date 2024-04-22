fed2959a8b060da136bc02f854b4e53cdc915535
Salman Farsi <sfjadi@gmail.com>
2024-04-22 07:14:50 -0400

    Refactor layout.tsx to include Header component and update Providers component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,17 +9,7 @@ export default async function Home() {
   const session = await auth();
   return (
     <div>
-      <form action={signIn.signIn}>
-        <Button type="submit" className="p-2">Sign In</Button>
-      </form>
-
-      <form action={signOut.signOut}>
-        <Button type="submit" className="p-2">Sign Out</Button>
-      </form>
-      {
-        session?.user ? <div>{JSON.stringify(session.user)}</div> : <div>Signed Out</div>
-      }
-      <Profile />
+      <span>Home Page</span>
     </div>
   );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,4 +7,77 @@ import {
   Input,
   Button,
   Avatar,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
 } from "@nextui-org/react";
+import { auth } from "@/auth";
+import * as actions from "@/actions";
+
+export default async function Header() {
+  const session = await auth();
+
+  let authContent: React.ReactNode;
+  
+  if (session?.user) {
+    authContent = (
+      <Popover placement="left">
+        <PopoverTrigger>
+          <Avatar src={session.user.image || ""} />
+        </PopoverTrigger>
+        <PopoverContent>
+          <form action={actions.signOut}>
+            <Button type="submit">Sign Out</Button>
+          </form>
+        </PopoverContent>
+      </Popover>
+    );
+  } else {
+    authContent = (
+      <>
+        <form action={actions.signIn}>
+          <NavbarItem>
+            <Button
+              type="submit"
+              color="secondary"
+              variant="bordered"
+              className="p-1"
+            >
+              Sign In
+            </Button>
+          </NavbarItem>
+        </form>
+
+        <form action={actions.signIn}>
+          <NavbarItem>
+            <Button
+              type="submit"
+              color="primary"
+              variant="bordered"
+              className="p-1"
+            >
+              Sign Up
+            </Button>
+          </NavbarItem>
+        </form>
+      </>
+    );
+  }
+
+  return (
+    <Navbar className="shadow mb-6">
+      <NavbarBrand>
+        <Link href="/" className="font-bold">
+          Brainstorming
+        </Link>
+      </NavbarBrand>
+      <NavbarContent justify="center">
+        <NavbarItem>
+          <Input placeholder="Search" />
+        </NavbarItem>
+      </NavbarContent>
+
+      <NavbarContent justify="end">{authContent}</NavbarContent>
+    </Navbar>
+  );
+}
